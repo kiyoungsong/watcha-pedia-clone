46c193e3dd539c0fc67d46910aceb14bd6570c10
ky.song <thdrldud369@gmail.com>
2024-02-05 22:30:00 +0900

    ♻️ [Refactor]: ShellRoute 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:watcha_pedia_clone/component/scaffold/scaffold_with_header_nav.dart';
+import 'package:watcha_pedia_clone/component/scaffold/scaffold_with_search_nav.dart';
 import 'package:watcha_pedia_clone/screen/movie.dart';
 import 'package:watcha_pedia_clone/screen/search.dart';
 import 'package:watcha_pedia_clone/screen/tv.dart';
@@ -8,18 +10,38 @@ void main() {
   runApp(const MyApp());
 }
 
-final GoRouter _router =
-    GoRouter(initialLocation: "/movie", routes: <RouteBase>[
-  GoRoute(
-    path: "/movie",
-    builder: (context, state) => const MovieScreen(),
-  ),
-  GoRoute(
-    path: "/tv",
-    builder: (context, state) => const TVScreen(),
-  ),
-  GoRoute(path: "/search", builder: (context, state) => const SearchScreen())
-]);
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
+
+final GoRouter _router = GoRouter(
+    initialLocation: "/",
+    navigatorKey: _rootNavigatorKey,
+    routes: <RouteBase>[
+      ShellRoute(
+          navigatorKey: _shellNavigatorKey,
+          builder: (context, state, child) => SaffoldWithHeaderNav(
+                child: child,
+              ),
+          routes: [
+            GoRoute(
+                path: "/", builder: (context, state) => const MovieScreen()),
+            GoRoute(
+              path: "/tv",
+              builder: (context, state) => const TVScreen(),
+            )
+          ]),
+      ShellRoute(
+          builder: (context, state, child) =>
+              SaffoldWithSearchNav(child: child),
+          routes: <RouteBase>[
+            GoRoute(
+              path: "/search",
+              builder: (context, state) => const SearchScreen(),
+            )
+          ])
+    ]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
