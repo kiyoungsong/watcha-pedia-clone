6f47f1cd9882efac8b2a68e7e5a2ee8eefd81e8f
ky.song <thdrldud369@gmail.com>
2024-02-06 22:23:06 +0900

    👥 [Feat]: API 관련 메타데이터 추가

---
+++ lib/service/meta.dart
@@ -0,0 +1,3 @@
+String baseUrl = "https://api.themoviedb.org/";
+String baseToken =
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzViMGY1NDYxNTBkZTU4NjIwMTQwYTUzMDg3NDMyOSIsInN1YiI6IjYwNGY2YmI3MTdiNWVmMDAyOGY0YmE3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Cu0N9KwKkJEbowjnwqsRvPn2ieuhKsahrVkPGHw7sH8";
