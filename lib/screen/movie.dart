b3c0528708709b080b58207d8cf24ee551c84037
ky.song <thdrldud369@gmail.com>
2024-02-13 20:43:57 +0900

    ✨ [Feat]: 영화 크로샐 추가

--- lib/screen/movie.dart
+++ lib/screen/movie.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class MovieScreen extends StatelessWidget {
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+class MovieScreen extends StatefulWidget {
   const MovieScreen({super.key});
+  @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
 
+class _MovieScreenState extends State<MovieScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text("movie");
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Text(
+          "현재상영중",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Container(
+          width: double.infinity,
+          child: CarouselSlider.builder(
+              itemCount: (imgList.length / 3).round(),
+              itemBuilder: (context, index, realIndex) {
+                final int first = index * 3;
+                final int second = first + 1;
+                final int third = first + 2;
+                return Row(
+                  children: [first, second, third].map((idx) {
+                    return Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 5),
+                            Image.network(imgList[idx], fit: BoxFit.fill),
+                            Text(
+                              "dddfdsfdsfdsfsfdfdsfsfdfdsfadfdsfs",
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 14),
+                            ),
+                            Text(
+                              "평점⭐3.5",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 14),
+                            )
+                          ]),
+                    ));
+                  }).toList(),
+                );
+              },
+              options: CarouselOptions(
+                  padEnds: false,
+                  enlargeCenterPage: false,
+                  viewportFraction: 1,
+                  enableInfiniteScroll: false,
+                  pageSnapping: false)),
+        ),
+      ]),
+    );
   }
 }
