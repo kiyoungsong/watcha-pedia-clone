be8d653a81fd578f386937cee422605fe0d1a921
ky.song <thdrldud369@gmail.com>
2024-02-20 19:07:42 +0900

    👥 [Feat]: titleObject, KeyEnum 추가

--- lib/service/meta.dart
+++ lib/service/meta.dart
@@ -1,5 +1,3 @@
-import 'package:watcha_pedia_clone/service/movie_list_service.dart';
-
 String baseUrl = "https://api.themoviedb.org/";
 String baseToken =
     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzViMGY1NDYxNTBkZTU4NjIwMTQwYTUzMDg3NDMyOSIsInN1YiI6IjYwNGY2YmI3MTdiNWVmMDAyOGY0YmE3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Cu0N9KwKkJEbowjnwqsRvPn2ieuhKsahrVkPGHw7sH8";
@@ -8,9 +6,13 @@ final baseHeaders = {
   'authorization': baseToken,
 };
 
+enum KeyEnum { nowPlaying, popular, topRated, upcoming, airingToday, onTheAir }
+
 const titleObject = {
   KeyEnum.nowPlaying: "현재상영중",
   KeyEnum.popular: "인기순",
   KeyEnum.topRated: "평점순",
   KeyEnum.upcoming: "개봉예정",
+  KeyEnum.airingToday: "오늘방송",
+  KeyEnum.onTheAir: "방송중"
 };
