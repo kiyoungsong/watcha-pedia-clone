8f6b1246254b7f3008da6c05922e62ed037903b2
ky.song <thdrldud369@gmail.com>
2024-02-28 22:35:57 +0900

    🚩 [Feat]: 주요 출연진 추가

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -1,10 +1,19 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:watcha_pedia_clone/model/detail.dart';
 import 'package:watcha_pedia_clone/service/detail_service.dart';
 import 'package:watcha_pedia_clone/service/meta.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  CarouselController controller = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     final id = GoRouterState.of(context).pathParameters["id"] ?? "0";
@@ -217,39 +226,165 @@ class DetailScreen extends StatelessWidget {
                             style: TextStyle(
                                 fontSize: 24, fontWeight: FontWeight.bold),
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          SizedBox(
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: 56,
-                                  height: 56,
-                                  decoration: BoxDecoration(
-                                      color: const Color.fromRGBO(
-                                          238, 238, 238, 1),
-                                      borderRadius: BorderRadius.circular(4)),
-                                  child: Center(
-                                      child: SvgPicture.asset(
-                                    "$baseAssetPath/user.svg",
-                                    fit: BoxFit.fill,
-                                    width: 24,
-                                    height: 24,
-                                    colorFilter: ColorFilter.mode(
-                                        Color.fromRGBO(107, 107, 107, 1),
-                                        BlendMode.srcIn),
-                                  )),
-                                )
-                              ],
-                            ),
-                          )
+                          getCarousel(info.cast, controller),
+                          // Stack(
+                          //   children: [
+                          //     ...getCredits(info.cast),
+                          //     Text("dd"),
+                          //     Text("123")
+                          //   ],
+                          // )
                         ]),
                   ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: SizedBox(
+                      child: Column(
+                    children: [
+                      SizedBox(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "코멘트",
+                                    style: TextStyle(
+                                        height: 1,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    "15",
+                                    style: TextStyle(
+                                        height: 1,
+                                        fontSize: 24,
+                                        color: Color.fromRGBO(255, 47, 110, 1)),
+                                  )
+                                ]),
+                          ),
+                          Text(
+                            "더보기",
+                            style: TextStyle(
+                                height: 1,
+                                fontSize: 16,
+                                color: Color.fromRGBO(255, 47, 110, 1)),
+                          )
+                        ],
+                      )),
+                      Text("content")
+                    ],
+                  )),
                 )
               ],
             );
           }
         });
   }
+}
+
+Widget getCarousel(List<CastModel> list, CarouselController controller) {
+  List<Widget> credits = getCredits(list);
+  return SizedBox(
+    height: 228,
+    child: CarouselSlider(
+        carouselController: controller,
+        items: credits,
+        options: CarouselOptions(
+            enableInfiniteScroll: false, height: 228, viewportFraction: 1)),
+  );
+}
+
+Widget getCredit(CastModel? item, bool isBorder) {
+  return item != null
+      ? SizedBox(
+          height: 76,
+          child: Row(
+            children: [
+              item.profilePath != null
+                  ? SizedBox(
+                      width: 56,
+                      height: 56,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Image.network(
+                            "https://media.themoviedb.org/t/p/original${item.profilePath}",
+                            fit: BoxFit.fill),
+                      ),
+                    )
+                  : Container(
+                      width: 56,
+                      height: 56,
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(238, 238, 238, 1),
+                          borderRadius: BorderRadius.circular(4)),
+                      child: Center(
+                          child: SvgPicture.asset(
+                        "$baseAssetPath/user.svg",
+                        fit: BoxFit.fill,
+                        width: 24,
+                        height: 24,
+                        colorFilter: const ColorFilter.mode(
+                            Color.fromRGBO(107, 107, 107, 1), BlendMode.srcIn),
+                      )),
+                    ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                    border: BorderDirectional(
+                        bottom: BorderSide(
+                            color: Color.fromRGBO(0, 0, 0, 0.08),
+                            style: isBorder
+                                ? BorderStyle.solid
+                                : BorderStyle.none))),
+                child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            item.name,
+                            style: const TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            item.character,
+                            style: const TextStyle(
+                                color: Color.fromRGBO(140, 140, 140, 1)),
+                          ),
+                        ])),
+              )),
+            ],
+          ),
+        )
+      : const SizedBox();
+}
+
+List<Widget> getCredits(List<CastModel> list) {
+  // 여기서 3개씩 묶어서 넘기자!
+  List<Widget> result = [];
+
+  for (int i = 0; i < list.length; i += 3) {
+    bool isLast = !(i + 1 < list.length) && !(i + 2 < list.length);
+    bool isSecondLast = !(i + 2 < list.length);
+    result.add(Column(
+      children: [
+        getCredit(list[i], isLast ? false : true),
+        getCredit(i + 1 < list.length ? list[i + 1] : null,
+            isSecondLast ? false : true),
+        getCredit(i + 2 < list.length ? list[i + 2] : null, false)
+      ],
+    ));
+  }
+  return result;
 }
