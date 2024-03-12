f5e91c57bd21750334168a439227654890024e87
ky.song <thdrldud369@gmail.com>
2024-03-12 20:59:36 +0900

    🚩 [Feat]: 리뷰 api 추가

--- lib/service/detail_service.dart
+++ lib/service/detail_service.dart
@@ -5,17 +5,26 @@ import 'dart:convert';
 
 class DetailService {
   Future<DetailModel> getDetail(int id) async {
-    final detailResponse = await get(
-        Uri.parse("$baseUrl/3/movie/$id?language=ko-KR"),
-        headers: baseHeaders);
+    final detailResponse =
+        await get(Uri.parse("$baseUrl/3/movie/$id"), headers: baseHeaders);
 
     final detailData = DetailModel.fromJson(json.decode(detailResponse.body));
 
-    final castResponse = await get(
-        Uri.parse("$baseUrl/3/movie/$id/credits?language=ko-KR"),
-        headers: baseHeaders);
-    final castData = CastModel.fromJson(json.decode(castResponse.body)["cast"]);
-    detailData.cast = castData;
+    final etcResponse = await Future.wait([
+      get(Uri.parse("$baseUrl/3/movie/$id/credits"), headers: baseHeaders),
+      get(Uri.parse("$baseUrl/3/movie/$id/reviews"), headers: baseHeaders),
+    ]);
+
+    for (int i = 0; i < etcResponse.length; i++) {
+      switch (i) {
+        case 0:
+          detailData.cast =
+              CastModel.fromJson(json.decode(etcResponse[i].body)["cast"]);
+        case 1:
+          detailData.review =
+              ReviewModel.formJson(json.decode(etcResponse[i].body)["results"]);
+      }
+    }
 
     return detailData;
   }
